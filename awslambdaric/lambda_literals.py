13df8923834576d9e7fb8c9da23a0cb4c6f4b9c5
Sean O Brien <briensea@amazon.com>
2024-02-13 14:38:19 +0000

    Update format of unhandled exception warning message.

--- awslambdaric/lambda_literals.py
+++ awslambdaric/lambda_literals.py
@@ -13,5 +13,5 @@ lambda_unhandled_exception_warning_message = str(
     "For functions using managed runtimes, runtime updates can be triggered by a function change, or can be applied automatically. "
     "To determine if the runtime has been updated, check the runtime version in the INIT_START log entry. "
     "If this error correlates with a change in the runtime version, you may be able to mitigate this error by temporarily rolling back to the previous runtime version. "
-    "For more information, see https://docs.aws.amazon.com/lambda/latest/dg/runtimes-update.html"
+    "For more information, see https://docs.aws.amazon.com/lambda/latest/dg/runtimes-update.html\r"
 )
