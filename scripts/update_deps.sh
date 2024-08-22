9425d49a9d7703b61292623bfa9f3896a8030c02
Quinn Sinclair <52372765+m-rph@users.noreply.github.com>
2024-08-22 14:42:20 +0100

    Patch libcurl configure.ac to work with later versions of autoconf (#168)
    Added a patch for libcurl/configure.ac to fix build issues with recent versions of autoconf
    
    This commit adds a patch for curl/configure.ac that replaces the inlined
    flags/conditionals with scoped ones.
    
    What's happening?
    
    1. We have build failures in later versions of autoconf.
        1. Why ? ./configure script for libcurl breaks during execution.
        2. Why ? Control flow structures are not correctly closed which
           results in a parse error.
        3. Why ? The structures in the m4 template in ./configure.ac for the
           7.83.1 release of libcurl rely on inlined scripts.
        4. Why [now]? The 2.72 update on autoconf changed the behaviour of
           the script generator.
    
    What do we do to ameliorate this?
    1. Corrected the config script
    2. Created a patch
    3. Apply the patch to the shipped curl dependency

--- scripts/update_deps.sh
+++ scripts/update_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-set -e
+set -x
 
 cd deps
 source versions
@@ -8,8 +8,17 @@ source versions
 # Clean up old files
 rm -f aws-lambda-cpp-*.tar.gz && rm -f curl-*.tar.gz
 
+
+LIBCURL="curl-${CURL_MAJOR_VERSION}.${CURL_MINOR_VERSION}.${CURL_PATCH_VERSION}"
+
 # Grab Curl
-wget -c "https://github.com/curl/curl/releases/download/curl-${CURL_MAJOR_VERSION}_${CURL_MINOR_VERSION}_${CURL_PATCH_VERSION}/curl-${CURL_MAJOR_VERSION}.${CURL_MINOR_VERSION}.${CURL_PATCH_VERSION}.tar.gz"
+wget -c "https://github.com/curl/curl/releases/download/curl-${CURL_MAJOR_VERSION}_${CURL_MINOR_VERSION}_${CURL_PATCH_VERSION}/$LIBCURL.tar.gz" -O - | tar -xz
+(
+  cd  $LIBCURL && \
+  patch -p1 < ../patches/libcurl-configure-template.patch
+)
+
+tar -czf $LIBCURL.tar.gz $LIBCURL --no-same-owner && rm -rf $LIBCURL
 
 # Grab aws-lambda-cpp
 wget -c https://github.com/awslabs/aws-lambda-cpp/archive/v$AWS_LAMBDA_CPP_RELEASE.tar.gz -O - | tar -xz
@@ -25,5 +34,5 @@ wget -c https://github.com/awslabs/aws-lambda-cpp/archive/v$AWS_LAMBDA_CPP_RELEA
 )
 
 ## Pack again and remove the folder
-tar -czvf aws-lambda-cpp-$AWS_LAMBDA_CPP_RELEASE.tar.gz aws-lambda-cpp-$AWS_LAMBDA_CPP_RELEASE --no-same-owner && \
+tar -czf aws-lambda-cpp-$AWS_LAMBDA_CPP_RELEASE.tar.gz aws-lambda-cpp-$AWS_LAMBDA_CPP_RELEASE --no-same-owner && \
   rm -rf aws-lambda-cpp-$AWS_LAMBDA_CPP_RELEASE
