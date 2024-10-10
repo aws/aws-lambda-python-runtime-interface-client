92cf1a3e20a8a2fad77006e060b57af3b097d4a0
Jeppe Fihl-Pearson <jeppe@memrise.com>
2024-10-10 09:23:26 +0100

    Remove support for Python 3.8

--- setup.py
+++ setup.py
@@ -84,7 +84,6 @@ setup(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -92,7 +91,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     ext_modules=get_runtime_client_extension(),
     test_suite="tests",
 )
