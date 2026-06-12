e9520b31e638c47412899cfb0907917534db94d4
Maxime David <maxday@amazon.com>
2026-06-12 12:57:43 +0000

    fix: headers

--- tests/test_lambda_runtime_log_utils.py
+++ tests/test_lambda_runtime_log_utils.py
@@ -1,5 +1,6 @@
 """
-Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
 """
 
 import logging
